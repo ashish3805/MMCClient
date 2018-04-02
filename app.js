c5b54854672c09a6341f2932465313bb23168967
Ashish Kumar Patel <ashish_3805@hotmail.com>
2018-04-02 22:22:58 +0530

    MMC lib ready

--- app.js
+++ app.js
@@ -1,33 +1,12 @@
-let Client = require('./lib/Client');
-let data = [
-  0,
-  0,
-  1,
-  4,
-  0.556,
-  0.08127,
-  107760,
-  164.755,
-  0.961,
-  0.0626,
-  0.000598,
-  0.0342,
-  0.0599,
-  1,
-  -9.163,
-  4,
-  0.79,
-  0.599,
-];
+let MMC = require('./lib/MMC');
+let config = require('./config');
 
-
-let client = new Client();
-client.connect();
-client.request(data).then((res)=>{
-  console.log(res);
-  client.disconnect();
-}, (err)=>{
-  console.log(err);
-  client.disconnect();
-});
-
+let mmc = new MMC(config);
+mmc.classifySong('Poker Face', 'Lady Gaga').then(
+  (result) => {
+    console.log(result);
+  },
+  (err) => {
+    console.log(err);
+  }
+);
